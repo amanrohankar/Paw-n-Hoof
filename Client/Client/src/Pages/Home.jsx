de5cf3076eb016fdcc062afb26258d20f334e267
gauravdhande20 <gauravdhande2022004@gmail.com>
2025-12-28 18:10:14 +0530

    SubSection is done

--- Client/Client/src/Pages/Home.jsx
+++ Client/Client/src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Herosec from '../Components/Herosec.jsx'
+import Subsec from '../Components/Subsec.jsx'
 import Footer from '../Components/Footer.jsx'
 
 function Home() {
   return (
     <>
       <Herosec />
+        <Subsec />
       <Footer />
     </>
   )
